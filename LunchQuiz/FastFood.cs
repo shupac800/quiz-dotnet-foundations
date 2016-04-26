5184b37ba186b05dd1c08eacc53fc7baeb0f319e
shupac800 <davidshuman@gmail.com>
2016-04-26 14:18:45 -0500

    added IProvidesLunch and IProvidesDinner interfaces

--- LunchQuiz/FastFood.cs
+++ LunchQuiz/FastFood.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 
 namespace LunchQuiz
 {
-    public class FastFood : Restaurant
+    public class FastFood : Restaurant,  IProvidesLunch, IProvidesDinner
     {
         public bool HasDriveThrough { get; set; }
+        public bool openForLunch { get; set; }
+        public bool openForDinner { get; set; }
+        public bool hasEarlyBirdSpecial { get; set; }
+        public bool hasExpressLunchMenu { get; set; }
 
         // constructor
         public FastFood()
         {
+            this.openForLunch = true;
+            this.hasExpressLunchMenu = false;
+            this.hasEarlyBirdSpecial = false;
+            this.openForDinner = true;
             this.MenuItems = new List<MenuItem>();
         }
     }
